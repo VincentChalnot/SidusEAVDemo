2700c727c0238f5fb18a14c5ccd50a3993e2d411
VincentChalnot <vincent.chalnot@gmail.com>
2015-12-15 00:06:32 +0100

    Basic admin working a little bit better, adding ability to create and edit

--- src/Sidus/EAVDemoBundle/Controller/DataController.php
+++ src/Sidus/EAVDemoBundle/Controller/DataController.php
@@ -2,13 +2,19 @@
 
 namespace Sidus\EAVDemoBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sidus\EAVDemoBundle\Entity\Data;
 use Sidus\EAVFilterBundle\Configuration\FilterConfigurationHandler;
+use Sidus\EAVModelBundle\Model\FamilyInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DataController extends Controller
 {
@@ -23,14 +29,14 @@ class DataController extends Controller
 
     /**
      * @Template()
-     * @param $familyCode
+     * @param string $familyCode
      * @param Request $request
      * @return array
      * @throws \Exception
      */
     public function listAction($familyCode, Request $request)
     {
-        $family = $this->get('sidus_eav_model.family_configuration.handler')->getFamily($familyCode);
+        $family = $this->getFamily($familyCode);
         $filterConfigName = 'sidus_eav_filter.configuration.' . $familyCode;
         $isDefault = false;
         if (!$this->has($filterConfigName)) {
@@ -73,5 +79,109 @@ class DataController extends Controller
             'datas' => $datas,
             'pager' => $pager,
         ];
+    }
+
+    /**
+     * @Template()
+     * @param string $familyCode
+     * @param Request $request
+     * @return Response
+     */
+    public function createAction($familyCode, Request $request)
+    {
+        $family = $this->getFamily($familyCode);
+        $class = $this->container->getParameter('sidus_eav_model.entity.data.class');
+        return $this->editAction($familyCode, new $class($family), $request);
+    }
+
+    /**
+     * @Template()
+     * @param string $familyCode
+     * @param Data $data
+     * @param Request $request
+     * @return array|RedirectResponse
+     */
+    public function editAction($familyCode, Data $data, Request $request)
+    {
+        $family = $this->getFamily($familyCode);
+        if ($family->getCode() !== $data->getFamilyCode()) {
+            throw new \UnexpectedValueException("Data family '{$data->getFamilyCode()}'' not matching admin family {$familyCode}");
+        }
+        $data->setValueClass($this->container->getParameter('sidus_eav_model.entity.value.class'));
+        $form = $this->createForm('sidus_data', $data);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getManager();
+            $em->persist($data);
+            $em->flush();
+
+            $this->addFlashMsg('success', 'sidus.admin.success');
+            return $this->redirectToRoute('sidus_eavdemo.data.edit', [
+                'familyCode' => $family->getCode(),
+                'id' => $data->getId(),
+            ]);
+        }
+
+        return [
+            'family' => $family,
+            'form' => $form->createView(),
+            'data' => $data,
+        ];
+    }
+
+    /**
+     * @param string $familyCode
+     * @return FamilyInterface
+     */
+    protected function getFamily($familyCode)
+    {
+        return $this->get('sidus_eav_model.family_configuration.handler')->getFamily($familyCode);
+    }
+
+    /**
+     * Get the current session or create a new one
+     * @return Session $session
+     */
+    protected function getSession()
+    {
+        $session = $this->get('session');
+        if (!$session) {
+            $session = new Session();
+            $session->start();
+        }
+
+        return $session;
+    }
+
+    /**
+     * Add a new flash message
+     * @param string $name
+     * @param mixed  $value
+     */
+    protected function addFlashMsg($name, $value)
+    {
+        $this->getSession()->getFlashBag()->add($name, $value);
+    }
+
+    /**
+     * Alias to get a doctrine repository
+     * @param  string           $persistentObjectName
+     * @param  string           $persistentManagerName
+     * @return EntityRepository
+     */
+    protected function getRepository($persistentObjectName, $persistentManagerName = null)
+    {
+        return $this->getManager($persistentManagerName)->getRepository($persistentObjectName);
+    }
+
+    /**
+     * Alias to return the entity manager
+     * @param null $persistentManagerName
+     * @return EntityManager
+     */
+    protected function getManager($persistentManagerName = null)
+    {
+        return $this->getDoctrine()->getManager($persistentManagerName);
     }
 }
